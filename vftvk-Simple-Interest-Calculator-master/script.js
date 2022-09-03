48baa759e140ff70534a2ce1abb5ea0ba4276598
Giovanni Reid <giovanni-mtl@hotmail.com>
2022-09-03 19:34:11 -0400

    Modify the JavaScript file
    Converted the 'No of Years' into the actual year in the future.
    
    Wrote a function that reads the value of the range slider and displays the value of the slider to the span tag element adjacent to the slider.
    
    Moved the const variables inside the compute function so that once clicked the function activates and returns the calculation

--- vftvk-Simple-Interest-Calculator-master/script.js
+++ vftvk-Simple-Interest-Calculator-master/script.js
@@ -1,6 +1,41 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+	//Create references to the input elements to validate
+	const principal = document.getElementById('principal').value;
+
+	//To add the focus funcition to the input field if the input is blank
+	const principals = document.getElementById('principal');
+
+	const rate = document.getElementById('rate').value;
+
+	const years = document.getElementById('years').value;
+
+	const interest = principal * years * rate / 100;
+
+	let year = new Date().getFullYear() + parseInt(years);
+
+	// Check if the input is greater than 0
+	const condition1 = principal > 0;
+	if (condition1) {
+		document.getElementById('result').innerText =
+			'If you deposit ' +
+			principal +
+			', ' +
+			'\n at an interest rate of ' +
+			rate +
+			'. ' +
+			'\n You will receive an amount of ' +
+			amount +
+			', ' +
+			'\n in the year ' +
+			year;
+	} else {
+		alert('Enter a positive number');
+		principals.focus();
+	}
 }
-        
+
+// Modifies the range percentage value
+function updateRate() {
+	let rateval = document.getElementById('rate').value;
+	document.getElementById('rate_val').innerText = rateval;
+}
